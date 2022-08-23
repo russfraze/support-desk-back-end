6424c4f77d68ac3d39cd393e11b528a8e61efd1f
Russell Fraze <russell.fraze@gmail.com>
2022-08-23 10:10:53 -0700

    server setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log('server')
+// bring in express with common syntax instead of import 
+const express = require('express')
+const express = require('dotenv').config()
+const PORT = process.env.PORT || 8000
+
+//initalize app
+const app = express()
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome to the Support Desk API' })
+})
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
