ee4c0df1c7744581f1fc872c97601319c9008117
Russell Fraze <russell.fraze@gmail.com>
2022-10-09 17:03:43 -0700

    userController

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 // bring in express with common syntax instead of import 
 const express = require('express')
 const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
 const PORT = process.env.PORT || 8000
 
 //initalize app
 const app = express()
+
+//stuff to do with installing body parser
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'Welcome to the Support Desk API' })
@@ -12,5 +17,7 @@ app.get('/', (req, res) => {
 
 //Routes
 app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
